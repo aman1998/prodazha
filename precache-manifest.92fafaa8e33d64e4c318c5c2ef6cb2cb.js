1df224f3b7381c8d560c25dc342ba598ea68d29c
amangeldi <1998-amangeldi@mail.ru>
2020-07-22 04:35:34 +0600

    Updates

---
+++ precache-manifest.92fafaa8e33d64e4c318c5c2ef6cb2cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc4aa6d218af53a315b91494565ca8ce",
+    "url": "/prodazha/index.html"
+  },
+  {
+    "revision": "357ed5bb84fdbc4e1992",
+    "url": "/prodazha/static/css/main.8012aa5b.chunk.css"
+  },
+  {
+    "revision": "1de05f4e77d0b43e5e5f",
+    "url": "/prodazha/static/js/2.653fee48.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/prodazha/static/js/2.653fee48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "357ed5bb84fdbc4e1992",
+    "url": "/prodazha/static/js/main.4d6c21f7.chunk.js"
+  },
+  {
+    "revision": "5dc554d3b8e56d38d3ae",
+    "url": "/prodazha/static/js/runtime-main.279097e8.js"
+  }
+]);
